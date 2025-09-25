e781c6d7f0a4002b887c89dfac0073d25f51051c
Austin Manuel <austin.manuel@protonmail.com>
2025-09-25 16:54:41 -0400

    Support end of string anchor

--- app/main.py
+++ app/main.py
@@ -16,60 +16,79 @@ def starting_class(pattern):
         print("Literal character found:", pattern[0])
         return pattern[0]
     
+
 def match(input_line, pattern):
+    anchor_start, anchor_end = False, False
+
     if pattern.startswith("^"):
-        if match_pattern(input_line, pattern[1:]):
-            return True
+        anchor_start = True
+        pattern = pattern[1:]
+    if pattern.endswith("$"):
+        anchor_end = True
+        pattern = pattern[:-1]
+
+    if anchor_start:
+        success, complete = match_pattern(input_line, pattern)
+        if success:
+            if anchor_end:
+                return success and complete
+            else:
+                return success
+    else: 
+        for index in range(len(input_line)):
+            substring = input_line[index:]
+            success, complete = match_pattern(substring, pattern)
+            if success:
+                if anchor_end:
+                    return success and complete
+                else:
+                    return success
         return False
-    else:
-        x = starting_class(pattern)
-        for char in input_line:
-            if match_pattern(char, x):
-                if match_pattern(input_line[input_line.index(char):], pattern):
-                    return True
+
 
 def match_pattern(input_line, pattern):
     if pattern == "":
-        return True
+        if input_line == "":
+            return True, True
+        else:
+            return True, False
     if input_line == "":
-        return False
+        return False, True
     
-
     if pattern.startswith(r"\d"):
         if input_line[0].isdigit():
-            if match_pattern(input_line[1:], pattern[2:]):
-                return True
-        return False
+            success, complete = match_pattern(input_line[1:], pattern[2:])
+            return success, complete
+        return False, False
+    
     elif pattern.startswith(r"\w"):
         if input_line[0].isalnum() or input_line[0] == "_":
-            if match_pattern(input_line[1:], pattern[2:]):
-                return True
-        return False
+            success, complete = match_pattern(input_line[1:], pattern[2:])
+            return success, complete
+        return False, False
+    
     elif pattern.startswith("["):
         if "]" not in pattern:
             raise RuntimeError("Unclosed character class")
         if pattern[1] == "^":
             chars = pattern[2:pattern.index("]")]
             if input_line[0] not in chars:
-                if match_pattern(input_line[1:], pattern[pattern.index("]")+1:]):
-                    return True
-            return False
+                success, complete = match_pattern(input_line[1:], pattern[pattern.index("]")+1:])
+                return success, complete
+            return False, False
         else:
             chars = pattern[1:pattern.index("]")]
             if input_line[0] in chars:
-                if match_pattern(input_line[1:], pattern[pattern.index("]")+1:]):
-                    return True
-            return False
+                success, complete = match_pattern(input_line[1:], pattern[pattern.index("]")+1:])
+                return success, complete
+            return False, False
+        
     elif pattern[0] == input_line[0]:
-        if match_pattern(input_line[1:], pattern[1:]):
-            return True
+        success, complete = match_pattern(input_line[1:], pattern[1:])
+        return success, complete
     else:
-        return False
-  
-  
-  #  else:
-   #     raise RuntimeError(f"Unhandled pattern: {pattern}")
-    
+        return False, False
+
 
 def main():
     pattern = sys.argv[2]
