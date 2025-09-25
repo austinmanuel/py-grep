539daefbe1c60a1a0b0e391d882c240ea271620f
Austin Manuel <austin.manuel@protonmail.com>
2025-09-25 14:21:38 -0400

    combine classes

--- app/main.py
+++ app/main.py
@@ -3,23 +3,43 @@ import sys
 # import pyparsing - available if you need it!
 # import lark - available if you need it!
 
-
 def match_pattern(input_line, pattern):
-    if len(pattern) == 1:
-        return pattern in input_line
-    elif pattern == "\\d":
-        return any(char.isdigit() for char in input_line)
-    elif pattern == "\\w":
-        return any(char.isalnum() or char == "_" for char in input_line)
-    elif pattern.startswith("[^") and pattern.endswith("]"):
-        xchars = pattern[2:-1]
-        return any(char not in xchars for char in input_line)
-    elif pattern.startswith("[") and pattern.endswith("]"):
-        chars = pattern[1:-1]
-        return any(char in chars for char in input_line)
-    else:
-        raise RuntimeError(f"Unhandled pattern: {pattern}")
+    if pattern == None:
+        return True
+    if input_line == None:
+        return False
+    
 
+    if pattern.startwith(r"\d"):
+        if input_line[0].isdigit():
+            match_pattern(input_line[1:], pattern[2:])
+        return False
+    elif pattern.startswith(r"\w"):
+        if input_line[0].isalnum() or input_line[0] == "_":
+            match_pattern(input_line[1:], pattern[2:])
+        return False
+    elif pattern.startswith("["):
+        if "]" not in pattern:
+            raise RuntimeError("Unclosed character class")
+        if pattern[1] == "^":
+            chars = pattern[2:pattern.index("]")]
+            if input_line[0] not in chars:
+                match_pattern(input_line[1:], pattern[pattern.index("]"):])
+            return False
+        else:
+            chars = pattern[1:pattern.index("]")]
+            if input_line[0] in chars:
+                match_pattern(input_line[1:], pattern[pattern.index("]"):])
+            return False
+    elif pattern[0] == input_line[0]:
+        return match_pattern(input_line[1:], pattern[1:])
+    else:
+        return False
+  
+  
+  #  else:
+   #     raise RuntimeError(f"Unhandled pattern: {pattern}")
+    
 
 def main():
     pattern = sys.argv[2]
@@ -29,11 +49,7 @@ def main():
         print("Expected first argument to be '-E'")
         exit(1)
 
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
-
-    # Uncomment this block to pass the first stage
-    if match_pattern(input_line, pattern):
+    if match_pattern(input_line, pattern, 1):
         exit(0)
     else:
         exit(1)
