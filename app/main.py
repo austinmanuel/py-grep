ff834e2e4a0c52da75d4e6b6dcfc7cfad80bf708
Austin Manuel <austin.manuel@protonmail.com>
2025-09-28 16:11:58 -0400

    Support single-line file in

--- app/main.py
+++ app/main.py
@@ -1,6 +1,48 @@
-import sys
+import sys, argparse
 
 # TODO Handle newlines in powershell
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog='pygrep',
+        description='A simplified Python implementation of grep'
+    )
+
+    parser.add_argument(
+        '-E', '--Expression',
+        required=True,
+        type=str,
+        help="The expression string to evaluate"
+    )
+    
+    parser.add_argument(
+        "src_file", 
+        nargs="?",
+        type=str,
+        help="File to perform the search on (optional)"
+    )
+
+    return parser.parse_args()
+
+
+def search_file(filename, pattern):
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                if match(line, pattern):
+                    print(line)
+                    return True
+    except FileNotFoundError:
+            print(f"Error: The file '{file}' was not found.")
+            return False
+        
+
+def search_stdin(input_line, pattern):
+    if match(input_line, pattern):
+        print(input_line)
+        return True
+    else:
+        return False
 
 def match(input_line: str, pattern: list) -> bool:
     anchor_start, anchor_end = False, False
@@ -90,7 +132,6 @@ def compile_pattern(pattern: str) -> list:
 def alternation_match(input_line: str, patterns: list):
     for pattern in patterns:
         if match(input_line, pattern):
-            print(f"Matched on pattern {pattern}, consuming {len(pattern)} characters of input")
             return True, len(pattern)
     return False, 0
 
@@ -100,26 +141,20 @@ def single_match(input_line: str, token) -> tuple[bool, int]:
     kind, value, _ = token
 
     if kind == "LITERAL" and input_line[0] == value:
-        print("Found literal: ", value)
         return True, 1
     if kind == "CLASS" and value == "\\d" and input_line[0].isdigit():
-        print("Found class match: ", value)
         return True, 1
     if kind == "CLASS" and value == "\\w" and input_line[0].isalnum() or input_line[0] == "_":
-        print("Found class match: ", value)
         return True, 1
     if kind == "CLASS" and value == "." and input_line[0]:
-        print("Found class match: ", value)
         return True, 1
     if kind == "GROUP":
         chars = value[1:-1]
         if chars[0] == "^":
             if input_line[0] not in chars[1:]:
-                print("Found neg group match: ", value)
                 return True, 1
         else:
             if input_line[0] in chars:
-                print("Found group match: ", value)
                 return True, 1
     return False, 0         
 
@@ -131,17 +166,13 @@ def match_pattern(input_line: str, pattern: list) -> tuple[bool, bool]:
 
     if kind == "ALT":
         compiled_patterns = [compile_pattern(x) for x in value.split("|")]
-        print(f"Sending {input_line} to alternation_match()")
         ok, consumed = alternation_match(input_line, compiled_patterns)
         if ok:
-            print(f"Input: {input_line}")
-            print(f"Sending back to match_pattern with input line: {input_line[consumed:]} and pattern: {pattern[1:]}")
             return match_pattern(input_line[consumed:], pattern[1:])
         else:
             return False, False
 
     if quant == "?":
-        print("Entered ? block")
         ok, consumed = single_match(input_line, (kind, value, None))
         if ok: 
             return match_pattern(input_line[consumed:], pattern[1:])
@@ -172,22 +203,20 @@ def match_pattern(input_line: str, pattern: list) -> tuple[bool, bool]:
     return False, False
 
 def main():
-    pattern = sys.argv[2]
-    input_line = sys.stdin.read()
+    args = parse_args()
 
-    if sys.argv[1] != "-E":
-        print("Expected first argument to be '-E'")
-        exit(1)
-
-    compiled_pattern = compile_pattern(pattern) 
-    print(compiled_pattern)
-
-    if match(input_line, compiled_pattern):
-        print("True")
-        exit(0)
+    compiled_pattern = compile_pattern(args.Expression) 
+    
+    if args.src_file:
+        if search_file(args.src_file, compiled_pattern):
+            exit(0)
+        else:
+            exit(1)
     else:
-        print("False")
-        exit(1)
+        if search_stdin(sys.stdin.read(), compiled_pattern):
+            exit(0)
+        else:
+            exit(1)
 
 
 if __name__ == "__main__":
